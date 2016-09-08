3ae4589dc347445921b349d8675afc715d0c2ac6
dmontag23 <dmontag23@gmail.com>
2016-09-07 23:33:50 -0600

    Simple UI with sieve of erathosthenes algorithm implemented as outlined from wikipedia

--- SieveOfEratosthenes/SieveOfEratosthenes/MainWindow.xaml.cs
+++ SieveOfEratosthenes/SieveOfEratosthenes/MainWindow.xaml.cs
@@ -24,5 +24,41 @@ namespace SieveOfEratosthenes
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int value;
+            if (int.TryParse(numberBox.Text, out value) && value >1)
+            {
+                List<int> primeNumbers = RunSieveOfEratosthenes(value);
+                displayPrimes.ItemsSource = primeNumbers;
+            }
+            else
+            {
+                MessageBox.Show("Please enter an integer greater than 1");
+                numberBox.Clear();
+                numberBox.Focus();
+            }
+        }
+
+        private List<int> RunSieveOfEratosthenes(int n)
+        {
+            List<int> primeNumbers = new List<int>();
+            List<bool> markNumbers = Enumerable.Repeat(true, n+1).ToList();  // create a new list that is indexed from 0 to n and initialized so every entry is true
+            for (int i = 2; i <= Math.Sqrt(n); i++)
+            {
+                if (markNumbers[i])
+                {
+                    for (int j = (int)Math.Pow(i,2); j <= n; j += i) markNumbers[j] = false;
+                }
+            }
+
+            for (int i = 2; i < markNumbers.Count; i++)
+            {
+                if (markNumbers[i]) primeNumbers.Add(i);
+            }
+
+            return primeNumbers;
+        }
     }
 }
