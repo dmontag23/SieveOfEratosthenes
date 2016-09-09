8e582c3e9ff954dfde50a80b3d695eab84c08a17
dmontag23 <dmontag23@gmail.com>
2016-09-09 02:17:27 -0600

    Optimized sieve algorithm and updated UI - added text to tell the user how many primes were found

--- SieveOfEratosthenes/SieveOfEratosthenes/MainWindow.xaml.cs
+++ SieveOfEratosthenes/SieveOfEratosthenes/MainWindow.xaml.cs
@@ -31,8 +31,6 @@ namespace SieveOfEratosthenes
             displayPrimes.ItemsSource = primeNumbers;    // bind the primeNumbers list to the listbox
         }
 
-
-
         private void button_Click(object sender, RoutedEventArgs e)
         {
             if (worker.IsBusy) return;    // ensures the current input is fully processed before accepting more input
@@ -40,11 +38,11 @@ namespace SieveOfEratosthenes
             int n;                        // initialize the int that will hold the input from the user
             if (int.TryParse(numberBox.Text, out n) && n >1)  // if the user enters an integer greater than 1
             {
-                progressBar.Maximum = n;      
+                progressBar.Maximum = n;
+                calcPrimesText.Text = "Calculating primes up to " + n.ToString() + "...";  // update the text box to let the users know that the algorithm is calculating 
                 worker.RunWorkerAsync(n);    // start the worker on another thread
             }
-            else MessageBox.Show("Error: Please enter an integer greater than 1.");       // if the user does not enter an integer greater than 1, show a simple error message
-
+            else MessageBox.Show("Error: Please enter an integer greater than 1.");         // if the user does not enter an integer greater than 1, show a simple error message
             numberBox.Clear();
             numberBox.Focus();   // ensures the user can always type in the numberBox without having to click on it
         }
@@ -57,7 +55,7 @@ namespace SieveOfEratosthenes
             int n = (int)e.Argument;                                           // take the number from the user and cast it to an int
 
             try {
-                List<bool> markNumbers = Enumerable.Repeat(true, n + 1).ToList();  // create a new boolean list that is indexed from 0 to n and initialized so every entry is true
+                List<bool> markNumbers = Enumerable.Repeat(true, n + 1).ToList();  // create a new boolean list that is indexed from 0 to n and initialize every entry to true
 
                 // start at the lowest prime (2) and check every number through root n
                 for (int i = 2; i <= Math.Sqrt(n); i++)
@@ -70,13 +68,16 @@ namespace SieveOfEratosthenes
                 }
 
                 // find the indicies of markNumbers that contain a true value (i.e. all of the numbers that have not been "crossed out")
-                for (int i = 2; i < markNumbers.Count; i++)
+                primeNumbers.Add(2);   // 2 will always appear in the list
+
+                // check the rest of the odd numbers to see if they are prime
+                for (int i = 3; i < markNumbers.Count; i+=2)
                 {
-                    if (markNumbers[i])        // if the number is true (i.e. not "crossed out")
+                    if (markNumbers[i])   // if the number is true (i.e. not "crossed out")
                     {
-                        primeNumbers.Add(i);                             // add the number to the list of primes
-                        worker.ReportProgress(i);                        // call the function to update the progress bar
-                        Thread.Sleep(1);                                 // allow this thread to sleep so the UI can be updated
+                        primeNumbers.Add(i);        // add the number to the list of primes
+                        worker.ReportProgress(i);   // call the function to update the progress bar
+                        Thread.Sleep(1);            // allow this thread to sleep so the UI can be updated
                     }
                 }
 
@@ -90,9 +91,14 @@ namespace SieveOfEratosthenes
         // called when the worker finishes executing
         private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            displayPrimes.Items.Refresh();                                                           // refresh the primes on the screen
-            if (primeNumbers.Count != 0) displayPrimes.BorderThickness = new Thickness(0, 5, 0, 5);  // add a border around the box of primes
-            progressBar.Value = 0;                                                                   // reset the progress bar
+            displayPrimes.Items.Refresh();                                                            // refresh the primes on the screen
+            if (primeNumbers.Count != 0) displayPrimes.BorderThickness = new Thickness(0, 5, 0, 5);   // add a border around the box of primes
+
+            // update the calculating textbox - takes care of both singluar and plural use of the word "prime"
+            if (primeNumbers.Count == 1) calcPrimesText.Text = primeNumbers.Count.ToString() + " prime up to " + progressBar.Value.ToString();
+            else calcPrimesText.Text = primeNumbers.Count.ToString() + " primes up to " + progressBar.Value.ToString();
+
+            progressBar.Value = 0;   // reset the progress bar                                                                          
         }
 
         // update the progress bar
